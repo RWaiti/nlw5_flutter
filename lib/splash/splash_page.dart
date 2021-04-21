6d06da8a667ffcacaa98d9e645609bee0890af37
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2021-04-20 23:14:40 -0300

    Update splash_page.dart

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -5,6 +5,15 @@ import 'package:nlw5_flutter/core/app_images.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
   }
 }
