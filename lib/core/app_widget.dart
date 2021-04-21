d6fd38c0cfba1997376eaa7132186c02a9ad4bf0
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2021-04-21 00:39:01 -0300

    Aula1 Finalizada
    Criado a SplashPage e o AppBar personalizado pro home.

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,12 +1,12 @@
-import 'package:nlw5_flutter/splash/splash_page.dart';
+import 'package:nlw5_flutter/core/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "DevQuiz",
-      home: SplashPage(),
+      title: "NLW5-DevQuiz",
+      home: HomePage(),
     );
   }
 }
